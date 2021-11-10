3c12b59ade711ff9b38d514855e9736ca6a26fb3
unknown <karthik.menon1600@gmail.com>
2021-11-10 14:12:24 +0530

    ready

--- app.js
+++ app.js
@@ -145,7 +145,7 @@ app.use((err, req, res, next) => {
 app.all("*", (req, res, next) => {
   next(new ExpressError("Page Not Found", 404));
 });
-
-app.listen(3000, () => {
-  console.log("Serving on port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Serving on ${port}`);
 });
